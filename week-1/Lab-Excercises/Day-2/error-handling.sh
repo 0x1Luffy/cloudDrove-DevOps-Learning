0ab114258dd6db7a25f878a09bb4713e4fc770d1
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-08-08 09:36:09 +0530

    error handling

---
+++ week-1/Lab-Excercises/Day-2/error-handling.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+
+
+## set -e command
+
+ set -e
+ echo "hello i am chetan "
+ hjsk
+ echo "hello i will not be printed ):"
+
+## set -x command
+
+ set -x
+ echo "now i am using -x flag"
+ ls /home/chetan
